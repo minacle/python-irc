f851565436a72cf46b5b0b92bdf6a9287754ce62
Mayu Laierlence <mayuki.minano@gmail.com>
2013-04-29 04:05:46 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from irc import irc
+import time
+
+network = irc.network("Ozinger")
+network.servers.append(irc.server("irc.ozinger.org", 6667))
+user = irc.user(nick="pyirctest")
+client = irc(network, user)
+
+def recv(irc, text):
+	print "RECV << %s" % text
+client.received += recv
+
+def sent(irc, text):
+	print "SENT >> %s" % text
+client.sent += sent
+
+def _001(irc, msg):
+	irc.send("join", "#pyirctest")
+client.rpl["001"] += _001
+
+def _join(irc, msg):
+	if msg.sender.nick == irc.user.nick:
+		irc.send("privmsg", msg.args[0], "Hello, World!")
+		time.sleep(5)
+		irc.send("quit")
+client.msg["JOIN"] += _join
+
+if __name__ == "__main__":
+	client.start()
+	client.join()
+
